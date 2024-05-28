a335cd47f8399b3754f5358b0b729612f5097c5c
judesonleo <judesonleo123@gmail.com>
2024-05-28 11:13:20 +0530

    first commit

---
+++ Example02_Switch1.ino
@@ -0,0 +1,101 @@
+#include <Arduino.h>
+#include <arduino_homekit_server.h>
+#include "wifi_info.h"
+
+#define LOG_D(fmt, ...)   printf_P(PSTR(fmt "\n") , ##__VA_ARGS__)
+
+// Pin definitions for switches
+#define PIN_SWITCH 5  // D1
+#define PIN_SWITCH2 4 // D2
+#define PIN_SWITCH3 0 // D3
+#define PIN_SWITCH4 2 // D4
+
+// Function prototypes
+void setup();
+void loop();
+void my_homekit_setup();
+void my_homekit_loop();
+void cha_switch_on_setter(const homekit_value_t value);
+void cha_switch_on_setter2(const homekit_value_t value);
+void cha_switch_on_setter3(const homekit_value_t value);
+void cha_switch_on_setter4(const homekit_value_t value);
+
+// Access your HomeKit characteristics defined in my_accessory.c
+extern "C" homekit_server_config_t config;
+extern "C" homekit_characteristic_t cha_switch_on;
+extern "C" homekit_characteristic_t cha_switch_on2;
+extern "C" homekit_characteristic_t cha_switch_on3;
+extern "C" homekit_characteristic_t cha_switch_on4;
+
+void setup() {
+  Serial.begin(115200);
+  wifi_connect(); // Defined in wifi_info.h
+  my_homekit_setup();
+}
+
+void loop() {
+  my_homekit_loop();
+  delay(10);
+}
+
+// HomeKit setup
+void my_homekit_setup() {
+  pinMode(PIN_SWITCH, OUTPUT);
+  digitalWrite(PIN_SWITCH, HIGH);
+  pinMode(PIN_SWITCH2, OUTPUT);
+  digitalWrite(PIN_SWITCH2, HIGH);
+  pinMode(PIN_SWITCH3, OUTPUT);
+  digitalWrite(PIN_SWITCH3, HIGH);
+  pinMode(PIN_SWITCH4, OUTPUT);
+  digitalWrite(PIN_SWITCH4, HIGH);
+
+  // Setters for each switch
+  cha_switch_on.setter = cha_switch_on_setter;
+  cha_switch_on2.setter = cha_switch_on_setter2;
+  cha_switch_on3.setter = cha_switch_on_setter3;
+  cha_switch_on4.setter = cha_switch_on_setter4;
+
+  // Initialize HomeKit
+  arduino_homekit_setup(&config);
+}
+
+// HomeKit loop
+void my_homekit_loop() {
+  arduino_homekit_loop();
+  static uint32_t next_heap_millis = 0;
+  const uint32_t t = millis();
+  if (t > next_heap_millis) {
+    // Show heap info every 5 seconds
+    next_heap_millis = t + 5 * 1000;
+    LOG_D("Free heap: %d, HomeKit clients: %d", ESP.getFreeHeap(), arduino_homekit_connected_clients_count());
+  }
+}
+
+// Setters for switch characteristics
+void cha_switch_on_setter(const homekit_value_t value) {
+  bool on = value.bool_value;
+  cha_switch_on.value.bool_value = on; // Sync the value
+  LOG_D("Switch 1: %s", on ? "ON" : "OFF");
+  digitalWrite(PIN_SWITCH, on ? LOW : HIGH);
+}
+
+void cha_switch_on_setter2(const homekit_value_t value) {
+  bool on = value.bool_value;
+  cha_switch_on2.value.bool_value = on; // Sync the value
+  LOG_D("Switch 2: %s", on ? "ON" : "OFF");
+  digitalWrite(PIN_SWITCH2, on ? LOW : HIGH);
+}
+
+void cha_switch_on_setter3(const homekit_value_t value) {
+  bool on = value.bool_value;
+  cha_switch_on3.value.bool_value = on; // Sync the value
+  LOG_D("Switch 3: %s", on ? "ON" : "OFF");
+  digitalWrite(PIN_SWITCH3, on ? LOW : HIGH);
+}
+
+void cha_switch_on_setter4(const homekit_value_t value) {
+  bool on = value.bool_value;
+  cha_switch_on4.value.bool_value = on; // Sync the value
+  LOG_D("Switch 4: %s", on ? "ON" : "OFF");
+  digitalWrite(PIN_SWITCH4, on ? LOW : HIGH);
+}
---
+++ my_accessory.c
@@ -0,0 +1,93 @@
+#include <homekit/homekit.h>
+#include <homekit/characteristics.h>
+
+void my_accessory_identify(homekit_value_t _value) {
+    printf("accessory identify\n");
+}
+
+// Switch characteristics
+homekit_characteristic_t cha_switch_on = HOMEKIT_CHARACTERISTIC_(ON, false);
+homekit_characteristic_t cha_switch_on2 = HOMEKIT_CHARACTERISTIC_(ON, false);
+homekit_characteristic_t cha_switch_on3 = HOMEKIT_CHARACTERISTIC_(ON, false);
+homekit_characteristic_t cha_switch_on4 = HOMEKIT_CHARACTERISTIC_(ON, false);
+
+homekit_accessory_t *accessories[] = {
+    // Switch 1
+    HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_switch, .services=(homekit_service_t*[]) {
+        HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "Switch1"),
+            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "Arduino HomeKit"),
+            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "0123456"),
+            HOMEKIT_CHARACTERISTIC(MODEL, "ESP8266/ESP32"),
+            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "1.0"),
+            HOMEKIT_CHARACTERISTIC(IDENTIFY, my_accessory_identify),
+            NULL
+        }),
+        HOMEKIT_SERVICE(SWITCH, .primary=true, .characteristics=(homekit_characteristic_t*[]){
+            &cha_switch_on,
+            NULL
+        }),
+        NULL
+    }),
+
+    // Switch 2
+    HOMEKIT_ACCESSORY(.id=2, .category=homekit_accessory_category_switch, .services=(homekit_service_t*[]) {
+        HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "Switch2"),
+            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "Arduino HomeKit"),
+            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "0123456"),
+            HOMEKIT_CHARACTERISTIC(MODEL, "ESP8266/ESP32"),
+            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "1.0"),
+            HOMEKIT_CHARACTERISTIC(IDENTIFY, my_accessory_identify),
+            NULL
+        }),
+        HOMEKIT_SERVICE(SWITCH, .primary=true, .characteristics=(homekit_characteristic_t*[]){
+            &cha_switch_on2,
+            NULL
+        }),
+        NULL
+    }),
+
+    // Switch 3
+    HOMEKIT_ACCESSORY(.id=3, .category=homekit_accessory_category_switch, .services=(homekit_service_t*[]) {
+        HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "Switch3"),
+            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "Arduino HomeKit"),
+            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "0123456"),
+            HOMEKIT_CHARACTERISTIC(MODEL, "ESP8266/ESP32"),
+            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "1.0"),
+            HOMEKIT_CHARACTERISTIC(IDENTIFY, my_accessory_identify),
+            NULL
+        }),
+        HOMEKIT_SERVICE(SWITCH, .primary=true, .characteristics=(homekit_characteristic_t*[]){
+            &cha_switch_on3,
+            NULL
+        }),
+        NULL
+    }),
+
+    // Switch 4
+    HOMEKIT_ACCESSORY(.id=4, .category=homekit_accessory_category_switch, .services=(homekit_service_t*[]) {
+        HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "Switch4"),
+            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "Arduino HomeKit"),
+            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "0123456"),
+            HOMEKIT_CHARACTERISTIC(MODEL, "ESP8266/ESP32"),
+            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "1.0"),
+            HOMEKIT_CHARACTERISTIC(IDENTIFY, my_accessory_identify),
+            NULL
+        }),
+        HOMEKIT_SERVICE(SWITCH, .primary=true, .characteristics=(homekit_characteristic_t*[]){
+            &cha_switch_on4,
+            NULL
+        }),
+        NULL
+    }),
+
+    NULL
+};
+
+homekit_server_config_t config = {
+    .accessories = accessories,
+    .password = "111-11-111"
+};
